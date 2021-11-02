0cc35a4bbcb0e94f58847f4568616da9b388f999
ImGabreuw <imgabreuw.dev@gmail.com>
2021-11-02 14:38:48 -0300

    Atualizaçao em 'instalaçoes': 'Novo plugin de syntax highlighting para Zsh'

--- instalacoes/oh-my-zsh.sh
+++ instalacoes/oh-my-zsh.sh
@@ -21,4 +21,7 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 ### Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+# Adicionar plugins=(fast-syntax-highlighting, zsh-autosuggestions, zsh-completions, ...)
+
+# Outra opção de "syntax highlighting": git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
