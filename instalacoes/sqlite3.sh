f70503d5b980b442734621398d55e92fa065c94f
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-07 15:28:38 -0300

    Instalações: 'Script de instalação do SQlite 3'

---
+++ instalacoes/sqlite3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### Referência: https://linuxhint.com/install-sqlite-ubuntu-linux-mint/
+
+### SQlite
+sudo apt update && sudo apt upgrade -y
+sudo apt install sqlite3
+
+### SQLite Browser
+sudo apt install sqlitebrowser
